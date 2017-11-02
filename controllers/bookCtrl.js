b08ac1907f54dce29a3591c9f8645be2e2145901
Adobe-Android <tngamerboy@gmail.com>
2017-11-02 16:05:16 -0500

    got the book objects coming back as expected

--- controllers/bookCtrl.js
+++ controllers/bookCtrl.js
@@ -16,11 +16,15 @@ module.exports.getAllBooks = (req, res, next) => {
     if (req.query.title) {
           
     request("https://www.googleapis.com/books/v1/volumes?q=${req.query.title}", function (error, response, body) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-      res.json(body);
-      // response.render('search_all_books', body);
+      // console.log('error:', error); // Print the error if one occurred
+      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body);
+      let parsed = JSON.parse(body);
+      res.json(parsed);
+      // console.log('body:', body[0]);
+      // Print the HTML for the Google homepage.
+      // res.json(body.items[0].volumeInfo.title);
+      // res.render('search_all_books', body[0]);
     });
     } else {
       res.render('search_all_books');
