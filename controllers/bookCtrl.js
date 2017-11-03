fa38e95b7040f0be768fb92de0460f08a992bc49
Adobe-Android <tngamerboy@gmail.com>
2017-11-03 11:52:39 -0500

    got book data like title and authors to the Pug template and fixed query issue in bookCtrl

--- controllers/bookCtrl.js
+++ controllers/bookCtrl.js
@@ -4,6 +4,7 @@ let request = require('request');
 
 
 module.exports.getAllBooks = (req, res, next) => {
+  console.log("getting to getAllBooks function")
   
   // Book.findAll({
     //   where: {
@@ -12,19 +13,12 @@ module.exports.getAllBooks = (req, res, next) => {
         //     }
         //   }
         // })
-        
+
     if (req.query.title) {
-          
-    request("https://www.googleapis.com/books/v1/volumes?q=${req.query.title}", function (error, response, body) {
-      // console.log('error:', error); // Print the error if one occurred
-      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body);
+      request(`https://www.googleapis.com/books/v1/volumes?q=${req.query.title}&maxResults=5`, function (error, response, body) {
       let parsed = JSON.parse(body);
-      res.json(parsed);
-      // console.log('body:', body[0]);
-      // Print the HTML for the Google homepage.
-      // res.json(body.items[0].volumeInfo.title);
-      // res.render('search_all_books', body[0]);
+      let books = parsed.items;
+      res.render('all_books', {books});
     });
     } else {
       res.render('search_all_books');
